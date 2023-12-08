5ef0e004fd7c2b0e522308ebae871fab30eeedf7
msteiner <maximilian.steiner@iteratec.com>
2023-12-08 14:14:19 +0100

    day7 part 2

--- day7/p1.ts
+++ day7/p1.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 
 // Specify the file path
 const filePath = 'input.txt';
-let cards=["A","K","Q","J","T","9","8","7","6","5","4","3","2"]
+let cards=["A","K","Q","T","9","8","7","6","5","4","3","2","J"]
 
 // Use 'fs.readFile' to read the file asynchronously
 fs.readFile(filePath, 'utf8', (err, data) => {  
@@ -23,12 +23,15 @@ fs.readFile(filePath, 'utf8', (err, data) => {
     })
     hands=sortHands(hands)
     let sum=0
-    console.log(hands)
+    //console.log(hands)
     let multiplier=hands.length
     hands.forEach(hand=>{
         sum+=hand.bid*multiplier
         multiplier--
     })
+    console.log(hands)
+    fs.writeFileSync('./sortedHands.txt', JSON.stringify(hands));
+
     console.log(sum)
 
 })
@@ -92,8 +95,31 @@ function getRank(hand:String){
     let rank=[...cards].map(card=>{
         let cardRegex= new RegExp(`${card}`,"g")
         let amount= [...hand.matchAll(cardRegex)]
-        return amount.length
-    }).filter(amount=>amount>0)
-    rank.sort((a,b)=>{if (a>b) return -1;if (a<b) return 1;return 0})
-    return rank
+        return {card,amount:amount.length}
+    }).filter(card=>card.amount>0)
+    rank.sort((a,b)=>{if (a.amount>b.amount) return -1;if (a.amount<b.amount) return 1;return 0})
+    let finalscore=rank.map(rank=>rank.amount)
+    rank.forEach((rank,index,ranks)=>{
+        if(rank.card=="J"){
+            console.log("score before J")
+
+            console.log(finalscore)
+
+            console.log(`found ${rank.amount} Js`)
+
+            finalscore.splice(index,1)
+            if(finalscore.length>0){
+                finalscore[0]+=rank.amount
+
+            }else{
+                finalscore=[rank.amount]
+
+            }
+            console.log(finalscore)
+
+        }
+
+    })
+    finalscore = finalscore.sort((a,b)=>{if (a>b) return -1;if (a<b) return 1;return 0})
+    return finalscore
 }
