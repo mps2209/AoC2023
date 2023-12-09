26ea212c280946edfd15664dd4280828d0bd1571
msteiner <maximilian.steiner@iteratec.com>
2023-12-09 16:26:45 +0100

    day9 part1

---
+++ day9/p1.ts
@@ -0,0 +1,39 @@
+import { time } from 'console';
+import * as fs from 'fs';
+import { retry } from 'rxjs';
+
+
+// Specify the file path
+const filePath = 'input.txt';
+
+// Use 'fs.readFile' to read the file asynchronously
+fs.readFile(filePath, 'utf8', (err, data) => {  
+    if (err) {
+        console.error(`Error reading file: ${err}`);
+        return;
+    }
+    let lines=data.split("\n")
+    let sequences=lines.map(line=>line.split(" ").map(n=>parseInt(n)))
+    let result = sequences.map(sequence=>getLastSequence(sequence)).reduce((a,b)=>a+b)
+    console.log(result)
+})
+
+function getLastSequence(sequence:number[]){
+    if (!allZeros(sequence)){
+        let nextSequence=calculateNextSequence(sequence);
+        return sequence[sequence.length-1] + getLastSequence(nextSequence) 
+    }
+    return 0   
+}
+function allZeros(sequence:number[]){
+    return sequence.every(n=>n==0)
+}
+function calculateNextSequence(sequence:number[]){
+    let nextSequence=sequence.map((n,index,array)=>{
+        if(index<array.length-1){
+            return (array[index+1]-n)
+        }
+    })
+    nextSequence.splice(nextSequence.length-1,1)
+    return nextSequence
+}
